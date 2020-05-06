0dbda0079c44878f5562806f81b9aee4c0ed4141
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-05-06 17:58:27 +0200

    clt : input sect act

--- src/app/_input/activity-sector-cust-input/activity-sector-cust-input.component.ts
+++ src/app/_input/activity-sector-cust-input/activity-sector-cust-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-activity-sector-cust-input',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-sector-cust-input.component.scss']
 })
 export class ActivitySectorCustInputComponent implements OnInit {
-
+  @Input() customer: any;
+  @Output() customerChange = new EventEmitter<any>();
+  activitySectorCtrl = new FormControl();
+  message = "";
   constructor() { }
 
   ngOnInit(): void {
+    this.activitySectorCtrl.setValue(this.customer.activitySector);
   }
 
+  onSubmit(){
+    if(this.activitySectorCtrl.value!=null){
+      console.log(this.message);
+      if(this.check()){
+        this.customer.activitySector = this.activitySectorCtrl.value;
+        this.customerChange.emit(this.customer);
+      }
+    }
+  }
+
+  check(): boolean{
+    const val = this.activitySectorCtrl.value as string;
+    if(val == this.customer.activitySector ){
+      this.message = "Le secteur d'activité est le même."
+      return false;
+    }
+    if(val.length<=0){
+      this.message = "Le secteur d'activité doit être rempli."
+      return false;
+    }
+    this.message = ""
+    return true;
+  }
 }
