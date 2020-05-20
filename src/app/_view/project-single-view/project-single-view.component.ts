036f2a9eda49c037e44c421b570cc7b4efa7c9ec
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-05-20 10:34:36 +0200

    image ds project view

--- src/app/_view/project-single-view/project-single-view.component.ts
+++ src/app/_view/project-single-view/project-single-view.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProjectSingleViewComponent implements OnInit {
   @Input() project;
+  src;
   constructor() { }
 
   ngOnInit(): void {
+    this.src = 'http://localhost:8080/hercules/missions/projects/picture/'+this.project.picture;
   }
 
 }
