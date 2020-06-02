b5f77402e154d2bb04856820c50b98ebf706bd62
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-06-02 17:56:29 +0200

    correction erreur console ngAfterViewInit + rafraichissement lors suppression mission

--- src/app/_page/missions/missions.component.ts
+++ src/app/_page/missions/missions.component.ts
@@ -1,20 +1,10 @@
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FormGroup } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { Role } from 'src/app/_enums/role.enum';
-import { MatDialog } from '@angular/material/dialog';
-import { YesNoDialogComponent } from 'src/app/dialog/yes-no/yes-no-dialog.component';
-import { ActivatedRoute } from '@angular/router';
 import { MissionService } from 'src/app/_services/mission.service';
 import { AuthService } from 'src/app/_services/auth.service';
-import { Component, OnInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { SelectionModel } from '@angular/cdk/collections';
-import { MatInput } from '@angular/material/input';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArrayMissionsViewComponent } from 'src/app/_view/array-missions-view/array-missions-view.component';
+import { MatDialog } from '@angular/material/dialog';
+import { YesNoDialogComponent } from 'src/app/dialog/yes-no/yes-no-dialog.component';
 
 
 
@@ -25,20 +15,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class MissionsComponent implements OnInit {
 
-  
-  missions:any[];
-  /*dataSource: MatTableDataSource<any>;
-  dataSourceProjects: MatTableDataSource<any>;
-  selection = new SelectionModel<any>(true, []);
-  NumberOfCheckboxesExceed = false;
-  NumberOfMaximumCheckboxes = 2;*/
-  
+  @ViewChild(ArrayMissionsViewComponent) arrayView: ArrayMissionsViewComponent;
+  missions: any[];
+
 
 
   constructor(
-    private _missionService : MissionService,
-    private _authService : AuthService
-  ) {}
+    private _missionService: MissionService,
+    private _authService: AuthService,
+    private _dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
 
@@ -52,170 +38,35 @@ export class MissionsComponent implements OnInit {
         console.log(err);
       }
     )
-   
+
 
   }
-
-
-
-  /*createDatasource(data) {
-    if (this.onlyMyMissionsChecked&&this.userIsManagerInclude) {
-      this.dataSource = new MatTableDataSource(data.filter((miss) => miss.consultant.manager.id == this.user.id));
-      if(this.onlyMyValidatedMissions&&this.userIsManagerInclude)
-    {
-      this.dataSource = new MatTableDataSource(data.filter((miss) => miss.sheetStatus == "ON_GOING" || miss.sheetStatus == "ON_WAITING"));
-    } 
-    }
-    else {
-      this.dataSource = new MatTableDataSource(data);
-      
-    }
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    
-    this.dataSource.sortingDataAccessor = (item, property) => {
-      switch(property) {
-        case 'title' : return item.lastVersion.title;
-        case 'consultant' : return item.consultant.firstname;
-        case 'customer': return item.customer.name;
-        case 'city' : return item.lastVersion.city;
-        case 'manager' : return item.consultant.manager.firstname;
-        case 'sheetStatus' : return item.sheetStatus;
-        case 'numberOfProjects' : return item.lastVersion.projects?.length;
-        default: return item[property];
-      }
-      
-    };
-
-    this.dataSource.filterPredicate = (data, filter: string) => {
-      const accumulator = (currentTerm, key) => {
-        return this.nestedFilterCheck(currentTerm, data, key);
-      };
-      const dataStr = Object.keys(data).reduce(accumulator, '').toLowerCase();
-      // Transform the filter by converting it to lowercase and removing whitespace.
-      const transformedFilter = filter.trim().toLowerCase();
-      return dataStr.indexOf(transformedFilter) !== -1;
-    };
-
-
-}
-
-
- 
-nestedFilterCheck(search, data, key) {
-  if (typeof data[key] === 'object') {
-    for (const k in data[key]) {
-      if (data[key][k] !== null) {
-        search = this.nestedFilterCheck(search, data[key], k);
-      }
-    }
-  } else {
-    search += data[key];
-  }
-  return search;
-}
-
-applyFilter(event: Event) {
-
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
-
-
-showOnlyMyMissions() {
-
- this.onlyMyMissionsChecked = !this.onlyMyMissionsChecked;
- this.createDatasource(this.missions);
-
- if(this.onlyMyMissionsChecked&&this.onlyMyValidatedMissions)
- {
- 
-  this.onlyMyValidatedMissions= !this.onlyMyValidatedMissions;
-  this.createDatasource(this.missions);
-  
- }
-
-}
-
-
-isAllSelected() {
-  const numSelected = this.selection.selected.length;
-
-  // const numRows = this.dataSource.data.length;
-  const numRowsMinusExcluded = this.dataSource.data
-    .filter(row => !(row.sheetStatus=='ON_WAITING' && row.versions?.length==null && row.lastVersion.versionDate==null))
-    .length;
-  
-  return numSelected === numRowsMinusExcluded;
-}
-
-
-masterToggle() {
-  this.isAllSelected() ?
-    this.selection.clear() :
-    // this.dataSource.data.forEach(row => this.selection.select(row));
-
-    this.dataSource.data.forEach(row => {
-      if (!(row.sheetStatus=='ON_WAITING' && row.versions?.length==null && row.lastVersion.versionDate==null)) {
-        this.selection.select(row);
-      }
+  openDeleteDialog(mission: any): void {
+    const dialog = this._dialog.open(YesNoDialogComponent, {
+      data: {
+        title: 'Supprimer la mission ' + mission.lastVersion.title + ' chez ' + mission.customer.name + '.',
+        message: 'Voulez-vous continuer ?',
+        yes: 'Supprimer la mission',
+        no: 'Annuler'
+      },
     });
 
-}
-
-showOnlyMyValidatedMissions() {
-  this.onlyMyValidatedMissions = !this.onlyMyValidatedMissions;
-  this.createDatasource(this.missions);
-}
-
-private delete(mission:any){
-  this._missionService.deleteMission(mission.id).subscribe(
-    ()=>{
-      this.ngOnInit();
-    },
-    (err) => {
-      console.log(err);
-    }
-  )
-}
-
-openDeleteDialog(element: any): void {
-  const dialog = this._dialog.open(YesNoDialogComponent, {
-    data: { 
-      title: 'Supprimer la mission '+element.lastVersion.title+' chez '+element.customer.name+'.' ,
-      message: 'Voulez-vous continuer ?',
-      yes:'Supprimer la mission',
-      no:'Annuler'
-    },
-  });
-
-  dialog.afterClosed().subscribe(
-    (result) => {
-      if(result){
-        this.delete(element);
-        this._snackBar.open('Mission '+element.lastVersion.title+' chez '+element.customer.name+' supprimée ', 'X', {duration: 2000});
+    dialog.afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          this.delete(mission);
+        }
       }
-    }
-  );
-}
-
-
-
-onClick(event,row) {
-  if(this.selection.selected.length>=this.NumberOfMaximumCheckboxes&&!(this.selection.isSelected(row))) {
-  event.preventDefault();
-  this._snackBar.open('Vous avez dépassé le nombre d\'éléments sélectionnés autorisé : '+this.NumberOfMaximumCheckboxes, 'X', {duration: 2000});
+    );
   }
-}
 
-SnackBarMessage()
-{
-  this._snackBar.open('Vous avez dépassé le nombre d\'éléments sélectionnés autorisé : '+this.NumberOfMaximumCheckboxes, 'X', {duration: 2000});
-}
-
-onClickProjects(event)
-{
-  event.preventDefault();
-}*/
-
+  delete(mission: any) {
+    this._missionService.deleteMission(mission.id).subscribe(
+      () => {
+        this.missions = this.missions.filter((m) => m.id !== mission.id);
+        this.arrayView.createDatasource(this.missions);
+      },
+      (err) => { }
+    )
+  }
 }
--- src/app/_view/array-missions-view/array-missions-view.component.ts
+++ src/app/_view/array-missions-view/array-missions-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, QueryList, ViewChild, ViewChildren, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, QueryList, ViewChild, ViewChildren, AfterViewInit, Output, EventEmitter, AfterContentInit, AfterContentChecked } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import { YesNoDialogComponent } from 'src/app/dialog/yes-no/yes-no-dialog.component';
@@ -27,7 +27,7 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 export class ArrayMissionsViewComponent implements OnInit, AfterViewInit {
   @Input() missions: any[];
   @Input() displayedColumns: string[] = ['select', 'title', 'consultant', 'customer', 'city', 'manager', 'numberOfProjects', 'sheetStatus'];
-
+  @Output() deleteEvent = new EventEmitter<any>();
   user;
   isAuthenticated = false;
   userIsAdmin = false;
@@ -36,9 +36,9 @@ export class ArrayMissionsViewComponent implements OnInit, AfterViewInit {
   checkBoxDisabled = true;
   onlyMyValidatedMissions = false;
   checked = false;
-  
-  userIsConsultantManager : boolean = false;
-  userId : number = null;
+
+  userIsConsultantManager: boolean = false;
+  userId: number = null;
 
   onlyMyMissionsChecked = true;
   dataSource: MatTableDataSource<any>;
@@ -46,10 +46,10 @@ export class ArrayMissionsViewComponent implements OnInit, AfterViewInit {
   selection = new SelectionModel<any>(true, []);
   NumberOfCheckboxesExceed = false;
   NumberOfMaximumCheckboxes = 2;
-  searchValue:string = null;
+  searchValue: string = null;
   advancedSearchEnabled = false;
 
-  innerDisplayedColumns: string[] = ['select','project-name','project-description'];
+  innerDisplayedColumns: string[] = ['select', 'project-name', 'project-description'];
 
 
 
@@ -60,12 +60,14 @@ export class ArrayMissionsViewComponent implements OnInit, AfterViewInit {
   isExpansionDetailRow = (index, row) => row.hasOwnProperty('detailRow');
 
 
+
+
   constructor(
-    private _authService : AuthService,
-    private _missionService : MissionService,
+    private _authService: AuthService,
+    private _missionService: MissionService,
     private _dialog: MatDialog,
     private _snackBar: MatSnackBar
-  ) {}
+  ) { }
 
   ngOnInit(): void {
 
@@ -82,43 +84,42 @@ export class ArrayMissionsViewComponent implements OnInit, AfterViewInit {
       this.userIsManager = this.userIsAdmin || this.user.role == 'MANAGER';
     }
 
-    
+    this.createDatasource(this.missions);
 
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.createDatasource(this.missions);
   }
 
 
 
-  createDatasource(data) {
-    if (this.onlyMyMissionsChecked&&this.userIsManagerInclude) {
+  public createDatasource(data) {
+    if (this.onlyMyMissionsChecked && this.userIsManagerInclude) {
       this.dataSource = new MatTableDataSource(data.filter((miss) => miss.consultant.manager.id == this.user.id));
-      if(this.onlyMyValidatedMissions&&this.userIsManagerInclude)
-    {
-      this.dataSource = new MatTableDataSource(data.filter((miss) => miss.sheetStatus == "ON_GOING" || miss.sheetStatus == "ON_WAITING"));
-    } 
+      if (this.onlyMyValidatedMissions && this.userIsManagerInclude) {
+        this.dataSource = new MatTableDataSource(data.filter((miss) => miss.sheetStatus == "ON_GOING" || miss.sheetStatus == "ON_WAITING"));
+      }
     }
     else {
       this.dataSource = new MatTableDataSource(data);
-      
+
     }
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    
+
     this.dataSource.sortingDataAccessor = (item, property) => {
-      switch(property) {
-        case 'title' : return item.lastVersion.title;
-        case 'consultant' : return item.consultant.firstname;
+      switch (property) {
+        case 'title': return item.lastVersion.title;
+        case 'consultant': return item.consultant.firstname;
         case 'customer': return item.customer.name;
-        case 'city' : return item.lastVersion.city;
-        case 'manager' : return item.consultant.manager.firstname;
-        case 'sheetStatus' : return item.sheetStatus;
-        case 'numberOfProjects' : return item.lastVersion.projects?.length;
+        case 'city': return item.lastVersion.city;
+        case 'manager': return item.consultant.manager.firstname;
+        case 'sheetStatus': return item.sheetStatus;
+        case 'numberOfProjects': return item.lastVersion.projects?.length;
         default: return item[property];
       }
-      
+
     };
 
     this.dataSource.filterPredicate = (data, filter: string) => {
@@ -132,141 +133,137 @@ export class ArrayMissionsViewComponent implements OnInit, AfterViewInit {
     };
 
 
-}
-
-
- 
-nestedFilterCheck(search, data, key) {
-  if (typeof data[key] === 'object') {
-    for (const k in data[key]) {
-      if (data[key][k] !== null) {
-        search = this.nestedFilterCheck(search, data[key], k);
-      }
-    }
-  } else {
-    search += data[key];
   }
-  return search;
-}
-
-applyFilter(event: Event) {
-
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
 
 
-showOnlyMyMissions() {
 
- this.onlyMyMissionsChecked = !this.onlyMyMissionsChecked;
- this.createDatasource(this.missions);
-
- if(this.onlyMyMissionsChecked&&this.onlyMyValidatedMissions)
- {
- 
-  this.onlyMyValidatedMissions= !this.onlyMyValidatedMissions;
-  this.createDatasource(this.missions);
-  
- }
-
-}
-
-
-isAllSelected() {
-  const numSelected = this.selection.selected.length;
-
-  // const numRows = this.dataSource.data.length;
-  const numRowsMinusExcluded = this.dataSource.data
-    .filter(row => !(row.sheetStatus=='ON_WAITING' && row.versions?.length==null && row.lastVersion.versionDate==null))
-    .length;
-  
-  return numSelected === numRowsMinusExcluded;
-}
-
-
-masterToggle() {
-  this.isAllSelected() ?
-    this.selection.clear() :
-    // this.dataSource.data.forEach(row => this.selection.select(row));
-
-    this.dataSource.data.forEach(row => {
-      if (!(row.sheetStatus=='ON_WAITING' && row.versions?.length==null && row.lastVersion.versionDate==null)) {
-        this.selection.select(row);
+  nestedFilterCheck(search, data, key) {
+    if (typeof data[key] === 'object') {
+      for (const k in data[key]) {
+        if (data[key][k] !== null) {
+          search = this.nestedFilterCheck(search, data[key], k);
+        }
       }
+    } else {
+      search += data[key];
+    }
+    return search;
+  }
+
+  applyFilter(event: Event) {
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+  showOnlyMyMissions() {
+
+    this.onlyMyMissionsChecked = !this.onlyMyMissionsChecked;
+    this.createDatasource(this.missions);
+
+    if (this.onlyMyMissionsChecked && this.onlyMyValidatedMissions) {
+
+      this.onlyMyValidatedMissions = !this.onlyMyValidatedMissions;
+      this.createDatasource(this.missions);
+
+    }
+
+  }
+
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+
+    // const numRows = this.dataSource.data.length;
+    const numRowsMinusExcluded = this.dataSource.data
+      .filter(row => !(row.sheetStatus == 'ON_WAITING' && row.versions?.length == null && row.lastVersion.versionDate == null))
+      .length;
+
+    return numSelected === numRowsMinusExcluded;
+  }
+
+
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      // this.dataSource.data.forEach(row => this.selection.select(row));
+
+      this.dataSource.data.forEach(row => {
+        if (!(row.sheetStatus == 'ON_WAITING' && row.versions?.length == null && row.lastVersion.versionDate == null)) {
+          this.selection.select(row);
+        }
+      });
+
+  }
+
+  showOnlyMyValidatedMissions() {
+    this.onlyMyValidatedMissions = !this.onlyMyValidatedMissions;
+    this.createDatasource(this.missions);
+  }
+
+  private delete(mission: any) {
+    this._missionService.deleteMission(mission.id).subscribe(
+      () => {
+        this.ngOnInit();
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
+
+  openDeleteDialog(element: any): void {
+    const dialog = this._dialog.open(YesNoDialogComponent, {
+      data: {
+        title: 'Supprimer la mission ' + element.lastVersion.title + ' chez ' + element.customer.name + '.',
+        message: 'Voulez-vous continuer ?',
+        yes: 'Supprimer la mission',
+        no: 'Annuler'
+      },
     });
 
-}
-
-showOnlyMyValidatedMissions() {
-  this.onlyMyValidatedMissions = !this.onlyMyValidatedMissions;
-  this.createDatasource(this.missions);
-}
-
-private delete(mission:any){
-  this._missionService.deleteMission(mission.id).subscribe(
-    ()=>{
-      this.ngOnInit();
-    },
-    (err) => {
-      console.log(err);
-    }
-  )
-}
-
-openDeleteDialog(element: any): void {
-  const dialog = this._dialog.open(YesNoDialogComponent, {
-    data: { 
-      title: 'Supprimer la mission '+element.lastVersion.title+' chez '+element.customer.name+'.' ,
-      message: 'Voulez-vous continuer ?',
-      yes:'Supprimer la mission',
-      no:'Annuler'
-    },
-  });
-
-  dialog.afterClosed().subscribe(
-    (result) => {
-      if(result){
-        this.delete(element);
-        this._snackBar.open('Mission '+element.lastVersion.title+' chez '+element.customer.name+' supprimée ', 'X', {duration: 2000});
+    dialog.afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          this.delete(element);
+          this._snackBar.open('Mission ' + element.lastVersion.title + ' chez ' + element.customer.name + ' supprimée ', 'X', { duration: 2000 });
+        }
       }
+    );
+  }
+
+
+
+  onClick(event, row) {
+    if (this.selection.selected.length >= this.NumberOfMaximumCheckboxes && !(this.selection.isSelected(row))) {
+      event.preventDefault();
+      this._snackBar.open(
+        'Vous avez dépassé le nombre d\'éléments sélectionnés autorisé : ' + this.NumberOfMaximumCheckboxes,
+        'X',
+        { duration: 2000 });
     }
-  );
-}
+  }
 
+  SnackBarMessage() {
+    if (this.selection.selected.length >= this.NumberOfMaximumCheckboxes) {
+      this._snackBar.open('Vous avez dépassé le nombre d\'éléments sélectionnés autorisé : ' + this.NumberOfMaximumCheckboxes, 'X', { duration: 2000 });
+    }
+  }
 
-
-onClick(event,row) {
-  if(this.selection.selected.length>=this.NumberOfMaximumCheckboxes&&!(this.selection.isSelected(row))) {
+  onClickProjects(event) {
     event.preventDefault();
-    this._snackBar.open(
-      'Vous avez dépassé le nombre d\'éléments sélectionnés autorisé : '+this.NumberOfMaximumCheckboxes, 
-      'X', 
-      {duration: 2000});
   }
-}
 
-SnackBarMessage()
-{
-  if(this.selection.selected.length>=this.NumberOfMaximumCheckboxes)
-  {
-  this._snackBar.open('Vous avez dépassé le nombre d\'éléments sélectionnés autorisé : '+this.NumberOfMaximumCheckboxes, 'X', {duration: 2000});
+  clearSearch() {
+    if (this.advancedSearchEnabled == false) {
+      this.searchValue = '';
+      console.log(this.searchValue.length, this.searchValue, typeof (this.searchValue));
+      this.advancedSearchEnabled = true;
+    }
+    else {
+      this.advancedSearchEnabled = false;
+    }
   }
-}
-
-onClickProjects(event)
-{
-  event.preventDefault();
-}
-
-clearSearch() { 
-  if (this.advancedSearchEnabled==false){
-  this.searchValue = '';
-  console.log(this.searchValue.length, this.searchValue, typeof(this.searchValue));
-  this.advancedSearchEnabled=true;
-  }
-  else {
-  this.advancedSearchEnabled=false;
-  }
-}
 
 }
