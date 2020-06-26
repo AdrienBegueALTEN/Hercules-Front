4e1e30a2fcd16823ec9c7cb0f313860a979ee4c9
MaximeFoltz <65277730+MaximeFoltz@users.noreply.github.com>
2020-06-26 15:48:23 +0200

    compodoc enum directive et header

--- src/app/_header/nav/nav-header.component.ts
+++ src/app/_header/nav/nav-header.component.ts
@@ -3,14 +3,27 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../_services/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+/**
+ * Component for the main header used for a connected user.
+ */
 @Component({
   selector: 'app-nav-header',
   templateUrl: './nav-header.component.html',
   styleUrls: ['../header.scss']
 })
 export class NavHeaderComponent implements OnInit {
+
+  /**
+   * Information of the user
+   */
   public user : any;
+  /**
+   * Boolean that indicates if the user is an admin
+   */
   public userIsAdmin : boolean = false;
+  /**
+   * Boolean that indicates if the user is a manager
+   */
   public userIsManager : boolean = false;
 
   constructor(
@@ -25,6 +38,9 @@ export class NavHeaderComponent implements OnInit {
     this.userIsManager = this.userIsAdmin || this._authService.userIsManager();
   }
 
+  /**
+   * Function activated when someone clicks on the "change password" option and that opens a dialog window for modifying the password.
+   */
   public onChangePassword() : void {
     this._dialogUtils.showChangePasswordDialog(this.user.id).afterClosed().subscribe(
       ok => {
@@ -35,6 +51,8 @@ export class NavHeaderComponent implements OnInit {
       }
     )
   }
-
+  /**
+   * Function activated when someone clicks on the "log out" option, it clears the session and will lead back the user to the connection screen.
+   */
   public onLogout() : void { this._authService.logout(); }
 }
