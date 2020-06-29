b31bb4dc6b2d6bf3faee91bef625a0c272c3a26f
MaximeFoltz <65277730+MaximeFoltz@users.noreply.github.com>
2020-06-29 13:55:00 +0200

    compodoc dossier helper

--- src/app/_helper/auth.interceptor.ts
+++ src/app/_helper/auth.interceptor.ts
@@ -5,10 +5,18 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
+/**
+ * Class that manages the http requests 
+ */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private _authService: AuthService) { }
 
+  /**
+   * Function that takes an http request, modifies its header and returns it as an Observable 
+   * @param req http request
+   * @param next structure to contain the modified http request
+   */
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let authReq = req;
     const token = this._authService.getToken();
@@ -19,6 +27,9 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 }
 
+/**
+ * "provide" field for an http request 
+ */
 export const authInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
 ];
